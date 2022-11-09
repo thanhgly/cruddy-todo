3360bd37f44e351ae9570d08cc2617e808f02b8c
achaidez <chaidez62@gmail.com>
2022-11-08 22:05:23 -0700

    Complete retrieving one todo

--- datastore/index.js
+++ datastore/index.js
@@ -40,12 +40,16 @@ exports.readAll = (callback) => {
 };
 
 exports.readOne = (id, callback) => {
-  var text = items[id];
-  if (!text) {
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback(null, { id, text });
-  }
+  fs.readFile(`${exports.dataDir}/${id}.txt`, (err, data) => {
+    if (err || data === undefined) {
+      return callback(new Error(`No item with id: ${id}`));
+    }
+    var text = data.toString();
+
+    callback(null, { id: id, text: text });
+
+
+  });
 };
 
 exports.update = (id, text, callback) => {

d2d3566585e4794ee0a6e0e8f00a698dda354334
achaidez <chaidez62@gmail.com>
2022-11-12 11:39:55 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -69,14 +69,13 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.rm(`${exports.dataDir}/${id}.txt`, (err) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
